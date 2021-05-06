681b9290c93e215fd3cc23860308087391ad1e89
olychkam <olkaaamartynova@gmail.com>
2021-05-06 18:49:39 +0300

    new project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './main/ui/App';
 import reportWebVitals from './reportWebVitals';
+import {store} from "./main/bll/store";
 
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
     <App />
+      </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
